85fb3f548527c2cab79c955fbc299cb88aa5a246
Tulip Pink <pinktulips@hotmail.com>
2019-01-11 11:06:43 -0800

    initial commit

---
+++ Lab-03-System-I.O/Lab-03-System-I.O/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab_03_System_I.O
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
